f1f9a91f93509117401595947c27acd8859e36e3
MmmarRTHa <m27842n@outlook.com>
2024-06-15 23:09:09 -0700

    - update: feature/job_listings - add Create jobs functionality

--- lib/dev_jobs_web/live/job_listings_live.ex
+++ lib/dev_jobs_web/live/job_listings_live.ex
@@ -1,43 +1,51 @@
 defmodule DevJobsWeb.JobListingsLive do
-    use DevJobsWeb, :live_view
+  alias DevJobs.JobListings
+  use DevJobsWeb, :live_view
 
-    alias DevJobs.JobListings.JobListing
+  alias DevJobs.JobListings.JobListing
 
+  def mount(_params, _session, socket) do
+    job_listing = %JobListing{}
+    changeset = JobListing.changeset(job_listing)
+    socket = assign(socket, job_listing: job_listing, changeset: changeset)
+    {:ok, socket}
+  end
 
-    def mount(_params, _session, socket) do
-        job_listing = %JobListing{}
-        changeset = JobListing.changeset(job_listing)
-        socket = assign(socket, job_listing: job_listing, changeset: changeset)
-        {:ok, socket}
-    end
+  def handle_event("validate", %{"job_listing" => params}, socket) do
+    changeset =
+      socket.assigns.job_listing
+      |> JobListing.changeset(params)
+      |> Map.put(:action, :validate)
 
-    def handle_event("validate", %{"job_listing" => params}, socket) do
-        IO.inspect("__________")
-        IO.inspect(params)
-        IO.inspect("__________")
-        changeset =
-            socket.assigns.job_listing
-            |> JobListing.changeset(params)
-            |> Map.put(:action, :insert)
+    {:noreply, assign(socket, changeset: changeset)}
+  end
 
+  def handle_event("create", %{"job_listing" => params}, socket) do
+    case JobListings.create_job_listing(params) do
+      {:ok, job_listing} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Job listing created successfully.")
+         |> push_navigate(to: ~p"/jobs")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
 
-
-    def render(assigns) do
-        ~H"""
-        <div>
-            <h1>Post a Job</h1>
-            <.form :let={f} for={@changeset} phx-change="validate" phx-submit="create">
-               <.input field={f[:title]} placeholder="Title" />
-                <.input field={f[:description]} placeholder="Description" />
-                <.input field={f[:location]} placeholder="Location" />
-                <.input field={f[:company]} placeholder="Company" />
-                <.input field={f[:salary]} placeholder="Salary" />
-                <.button>Post Job</.button>
-            </.form>
-        </div>
-        """
-    end
-
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1>Post a Job</h1>
+      <.form :let={f} for={@changeset} phx-change="validate" phx-submit="create" class="space-y-6">
+        <.input field={f[:title]} placeholder="Title" />
+        <.input field={f[:description]} placeholder="Description" />
+        <.input field={f[:location]} placeholder="Location" />
+        <.input field={f[:company]} placeholder="Company" />
+        <.input field={f[:salary]} placeholder="Salary" />
+        <.button>Post Job</.button>
+      </.form>
+    </div>
+    """
+  end
 end
