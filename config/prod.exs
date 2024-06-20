bf6b1b1f8baacb0365db1415ea9d5882f57b9004
MmmarRTHa <m27842n@outlook.com>
2024-06-20 00:31:01 -0700

    - update: main - fix prod endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dev_jobs, DevJobsWeb.Endpoint,
+url: [host: "https://dev-jobs.gigalixirapp.com/", scheme: "https", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
+check_origin: ["https://dev-jobs.gigalixirapp.com"],
 cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
