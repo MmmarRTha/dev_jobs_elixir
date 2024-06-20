ea630678ef570fc6a1d36e75232187b07532f215
MmmarRTHa <m27842n@outlook.com>
2024-06-19 23:30:38 -0700

    - update: feature/components - add phoenix components, refactor

--- lib/dev_jobs_web/live/job_listings_live.ex
+++ lib/dev_jobs_web/live/job_listings_live.ex
@@ -1,7 +1,8 @@
 defmodule DevJobsWeb.JobListingsLive do
-  alias DevJobs.JobListings
   use DevJobsWeb, :live_view
+  import DevJobsWeb.JobListingsLive.Components
 
+  alias DevJobs.JobListings
   alias DevJobs.JobListings.JobListing
 
   def mount(_params, _session, socket) do
@@ -60,59 +61,14 @@ defmodule DevJobsWeb.JobListingsLive do
       class="px-4 py-2 uppercase rounded-full bg-fuchsia-500 hover:bg-fuchsia-600"
       phx-click={JS.patch(~p"/new") |> show_modal("job-form-modal")}
     >
-      <%= gettext("Post a new Job") %>
+      Post a new Job
     </.button>
-
-    <.modal id="job-form-modal" show={@live_action in [:new, :edit]} on_cancel={JS.patch(~p"/")}>
-      <div>
-        <h1>Post a Job</h1>
-        <.form :let={f} for={@changeset} phx-change="validate" phx-submit="save" class="space-y-6">
-          <.input type="text" label="Title:" field={f[:title]} placeholder="Title" />
-          <.input type="text" label="Description:" field={f[:description]} placeholder="Description" />
-          <.input type="text" label="Location:" field={f[:location]} placeholder="Location" />
-          <.input type="text" label="Company:" field={f[:company]} placeholder="Company" />
-          <.input type="number" label="Salary:" field={f[:salary]} placeholder="Salary" />
-          <.button class="bg-fuchsia-500 hover:bg-fuchsia-600">Post Job</.button>
-        </.form>
-      </div>
-    </.modal>
-
-    <div>
-      <h1 class="text-xl font-bold text-center uppercase">Job Listings</h1>
-      <div class="container">
-        <ul class="space-y-6">
-          <li :for={job_listing <- @job_listings} class="border-b last:border-b-0 ">
-            <strong><%= job_listing.title %></strong>
-            <p>
-              <span class="text-sm text-gray-600 label">Description: </span><%= job_listing.description %>
-            </p>
-            <p>
-              <span class="text-sm text-gray-600 label">Location: </span><%= job_listing.location %>
-            </p>
-            <p>
-              <span class="text-sm text-gray-600 label">Company: </span><%= job_listing.company %>
-            </p>
-            <p><span class="text-sm text-gray-600 label">Salary: </span>$<%= job_listing.salary %></p>
-            <div class="pb-2 text-right">
-              <.button
-                class="py-1 bg-sky-500 hover:bg-sky-600"
-                phx-click={JS.patch(~p"/edit/#{job_listing.id}") |> show_modal("job-form-modal")}
-              >
-                Update
-              </.button>
-              <.button
-                class="py-1 bg-red-500 hover:bg-red-600"
-                phx-click="delete"
-                phx-value-id={job_listing.id}
-                data-confirm="Are you sure to delete this job?"
-              >
-                Delete
-              </.button>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <.job_listing_rows job_listings={@job_listings} />
+    <.job_form_modal
+      :if={@live_action in [:new, :edit]}
+      changeset={@changeset}
+      job_listing={@job_listing}
+    />
     """
   end
 
