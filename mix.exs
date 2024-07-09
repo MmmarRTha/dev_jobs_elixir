6fac4459a1b603a77e33e9750d2445cc4402f99a
MmmarRTHa <m27842n@outlook.com>
2024-07-09 00:23:11 -0700

    - wip: feature/send_emails_production - wip

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule DevJobs.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:timex, "~> 3.7"}
+      {:timex, "~> 3.7"},
+      {:resend, "~> 0.4"},
     ]
   end
 
