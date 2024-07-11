ac298db0c36aed26da1ae7b1f1f488fb2a050ffb
MmmarRTHa <m27842n@outlook.com>
2024-07-10 19:07:36 -0700

    - wip: bugfux/magic_link_email - working email link

--- lib/dev_jobs/user_email.ex
+++ lib/dev_jobs/user_email.ex
@@ -18,7 +18,7 @@ defmodule DevJobs.UserEmail do
   end
 
   def magic_link_email(user, magic_link_url) do
-    subject = "Sign in to DevJobs"
+    subject = "Sign in to Dev Jobs"
     body_email = email_body(user, magic_link_url)
 
     if System.get_env("RESEND_API_KEY") do
@@ -29,19 +29,20 @@ defmodule DevJobs.UserEmail do
   end
 
   def email_body(user, magic_link_url) do
-      """
-      Hello #{user.email}
+    """
+    Hello #{user.email}
 
-      <p style="font-size:16px;line-height:26px;margin:16px 0" >Here you will find a magic link to sign in to our DevJobs platform. </p>
+    Here you will find a magic link to sign in to our Dev Jobs platform.
 
-        <a class="text-gray-900 bg-gradient-to-r from-teal-200 via-teal-400 to-teal-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" href="#{magic_link_url}">Click here to Sign In</a>
+      #{magic_link_url}
 
-      <p style="font-size:16px;line-height:26px;margin:16px 0">If you didn&#x27;t request this, please ignore this email.</p>
-      """
+    If you didn't request this, please ignore this email.
+    """
   end
 
   defp send_email_using_resend(%{user: user, subject: subject, body_email: body_email}) do
     client = Resend.client(api_key: "re_65NjJqey_GWbpGHzEXXboKYp8RSGdFsfe")
+
     Resend.Emails.send(client, %{
       from: @from,
       to: [user.email],
