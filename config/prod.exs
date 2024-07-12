65d72e1f8e54d218e4630b4a467d5a5ff627c281
MmmarRTHa <m27842n@outlook.com>
2024-07-12 01:57:26 -0700

    add minor change

--- config/prod.exs
+++ config/prod.exs
@@ -8,15 +8,15 @@ import Config
 host = System.get_env("PHX_HOST") || "devjob-elixir.me"
 gig_host = System.get_env("GIG_HOST") || "devjob-elixir.me"
 
-config :dev_jobs, DevJobs.Mailer,
-  adapter: Resend.Swoosh.Adapter,
-  api_key: System.get_env("RESEND_API_KEY") || raise("RESEND_API_KEY is not set")
-
 config :dev_jobs, DevJobsWeb.Endpoint,
-  url: [host: "devjob-elixir.me", scheme: "https", port: 443],
+  url: [host: host, scheme: "https", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   check_origin: [host, gig_host],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :dev_jobs, DevJobs.Mailer,
+  adapter: Resend.Swoosh.Adapter,
+  api_key: System.get_env("RESEND_API_KEY") || raise("RESEND_API_KEY is not set")
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: DevJobs.Finch
